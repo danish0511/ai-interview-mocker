23e918314d842e9f90f366b748f91cc781fdc7c0
Danish <113869650+danish0511@users.noreply.github.com>
2024-07-06 00:24:44 +0530

    Update AddNewInterview.jsx

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -38,7 +38,7 @@ function AddNewInterview() {
 
         const result=await chatSession.sendMessage(InputPrompt);
         const MockJsonResp=(result.response.text()).replace('```json','').replace('```','')
-        console.log(JSON.parse(MockJsonResp));
+        console.log(MockJsonResp);
         setJsonResponse(MockJsonResp);
 
         if(MockJsonResp)
